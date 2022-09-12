805e27f62a9ef35aa9859bc75487cda3e4817066
Mahmoud Abd Alziem <mbdalzym376@gmail.com>
2022-09-12 19:18:04 +0200

    Update Rds Connection With Screts And ConfigMap

--- database/knexfile.js
+++ database/knexfile.js
@@ -1,9 +1,10 @@
+console.log(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASS, process.env.DB_NAME)
 const development = {
     client: 'mysql2',
     connection: {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: process.env.DB_PASS,
+        password: "azima1234" || process.env.DB_PASS,
         database: process.env.DB_NAME
     },
     pool: {
